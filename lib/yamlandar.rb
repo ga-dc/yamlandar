1f4df1e465f41fddf4179b0a8d139998ba102868
Jesse Shawl <jesse@jshawl.com>
2016-08-25 10:21:24 -0400

    allow titles for url

--- lib/yamlandar.rb
+++ lib/yamlandar.rb
@@ -3,6 +3,10 @@ require 'yaml'
 require 'erb'
 
 class Yamlandar < Sinatra::Base
+  def self.urls string_or_hash
+    html = ''
+    html
+  end
   def self.generate
     weeknum = 1
     @html = ""
@@ -24,21 +28,33 @@ class Yamlandar < Sinatra::Base
       day["day"].each do |events|
         events.each do |time, details|
           title = details["title"]
-          urls = details["url"].split(",")
+	  if details["url"].class == String
+	    urls = details["url"].split(",")
+	  elsif details["url"].class == Array
+	    urls = details["url"]
+	  end
           lead = details["lead"]
           support = details["support"]
           @html += "<div class='event'>"
           @html += "<h3>"
           if urls[0] != ""
-    	@html += "<a href='#{urls[0]}'>#{title}</a>"
+	    @html += "<a href='#{urls[0]}'>#{title}</a>"
           else
             @html += "#{title}"
           end
           @html += "</h3><small>#{time}</small>"
+
+
           if urls.length > 1
             @html += "<ul>"
             urls.each do |url|
-    	  @html += "<li><a href='#{url}'>#{url}</a></li>"
+	      if url.class == Hash
+                url.each_pair do |title, href|
+		  @html += "<li><a href='#{href}'>#{title}</a></li>"
+		end
+	      else
+		@html += "<li><a href='#{url}'>#{url}</a></li>"
+	      end
     	end
             @html += "</ul>"
           end
